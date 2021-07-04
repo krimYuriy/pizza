ed721eaf9c8b0a9ef3c2f01db0e3747aad4fde73
krimYuriy <yator0o@gmail.com>
2021-07-04 13:44:39 +0300

    finish cart

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,4 +1,4 @@
-import { ADD_PIZZAS, REMOVE_CART } from "../types";
+import { ADD_PIZZAS, REMOVE_CART, REMOVE_PIZZA_ITEM, INCREASE_PIZZA_COUNT, DECREASE_PIZZA_COUNT } from "../types";
 
 export const setPizza = obj => ({
    type: ADD_PIZZAS,
@@ -7,4 +7,19 @@ export const setPizza = obj => ({
 
 export const removePizza = () => ({
    type: REMOVE_CART
+})
+
+export const removePizzaItem = id => ({
+   type: REMOVE_PIZZA_ITEM,
+   payload: id
+})
+
+export const increasePizzaCount = id => ({
+   type: INCREASE_PIZZA_COUNT,
+   payload: id
+})
+
+export const decreasePizzaCount = id => ({
+   type: DECREASE_PIZZA_COUNT,
+   payload: id
 })
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_PIZZAS, REMOVE_CART } from "../types"
+import { ADD_PIZZAS, DECREASE_PIZZA_COUNT, INCREASE_PIZZA_COUNT, REMOVE_CART, REMOVE_PIZZA_ITEM } from "../types"
 
 const initialState = {
    items: {},
@@ -6,10 +6,11 @@ const initialState = {
    totalPizzasCount: 0
 }
 
+const getPrice = (arr) => arr.reduce((total, item) => total + item.price, 0)
+
 const cartReducer = (state = initialState, action) => {
    switch (action.type) {
       case ADD_PIZZAS: {
-         const getPrice = (arr) => arr.reduce((total, item) => total + item.price, 0)
 
          const currentItem = !state.items[action.payload.id]
             ? [action.payload]
@@ -44,6 +45,67 @@ const cartReducer = (state = initialState, action) => {
             totalPizzasPrice: 0,
             totalPizzasCount: 0
          }
+      case REMOVE_PIZZA_ITEM: {
+         const newItems = {
+            ...state.items
+         }
+         const currenPrice = newItems[action.payload].pizzaPrice
+         const currenCount = newItems[action.payload].pizzas.length
+         delete newItems[action.payload]
+
+         return {
+            ...state,
+            items: newItems,
+            totalPizzasPrice: state.totalPizzasPrice - currenPrice,
+            totalPizzasCount: state.totalPizzasCount - currenCount
+         }
+      }
+      case INCREASE_PIZZA_COUNT: {
+         const currentPizzas = [...state.items[action.payload].pizzas, state.items[action.payload].pizzas[0]]
+         const currentPizzaPrice = state.items[action.payload].pizzas[0].price
+
+
+         const newItems = {
+            ...state.items,
+            [action.payload]: {
+               pizzas: currentPizzas,
+               pizzaPrice: getPrice(currentPizzas)
+            }
+         }
+
+         return {
+            ...state,
+            items: newItems,
+            totalPizzasPrice: state.totalPizzasPrice + currentPizzaPrice,
+            totalPizzasCount: state.totalPizzasCount + 1
+         }
+      }
+      case DECREASE_PIZZA_COUNT: {
+
+         const currentPizzaPrice = state.items[action.payload].pizzas.length > 1 ? state.items[action.payload].pizzas[0].price : 0
+
+         const currentPizzas = state.items[action.payload].pizzas.length > 1
+            ? state.items[action.payload].pizzas.slice(1)
+            : [state.items[action.payload].pizzas[0]]
+
+
+         const newItems = {
+            ...state.items,
+            [action.payload]: {
+               pizzas: currentPizzas,
+               pizzaPrice: getPrice(currentPizzas)
+            }
+         }
+
+         const count = Object.keys(newItems).reduce((total, key) => total + newItems[key].pizzas.length, 0)
+
+         return {
+            ...state,
+            items: newItems,
+            totalPizzasPrice: state.totalPizzasPrice - currentPizzaPrice,
+            totalPizzasCount: count
+         }
+      }
       default:
          return state
    }
